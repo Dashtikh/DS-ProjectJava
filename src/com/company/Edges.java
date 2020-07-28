96284a5cc7ab3727fadc3c34d60732a62a4a32b9
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2020-07-28 11:58:08 +0430

    v and e defined

---
+++ src/com/company/Edges.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Edges {
+    private int fromId;
+    private int toId;
+    private int relation;
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getToId() {
+        return toId;
+    }
+
+    public void setToId(int toId) {
+        this.toId = toId;
+    }
+
+    public int getRelation() {
+        return relation;
+    }
+
+    public void setRelation(int relation) {
+        this.relation = relation;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,23 @@
 package com.company;
 
 import com.sun.javafx.css.parser.LadderConverter;
+import com.sun.javafx.geom.Edge;
 
+import javax.swing.plaf.IconUIResource;
+import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static Person person = new Person();
     static ArrayList<Person> personArrayList = new ArrayList<Person>();
+    static ArrayList<Edges> edgesArrayList = new ArrayList<Edges>();
+
 
     public static void main(String[] args) {
+
         int menu;
-        int id;
+        int id = 0;
         String fName;
         String lName;
         boolean sex;
@@ -31,8 +37,7 @@ public class Main {
                     Scanner scanner5 = new Scanner(System.in);
                     Scanner scanner6 = new Scanner(System.in);
                     Scanner scanner7 = new Scanner(System.in);
-                    System.out.println("insert the id");
-                    id = scanner1.nextInt();
+                    System.out.println("the id is=" + id);
                     System.out.println("insert the firstname");
                     fName = scanner2.nextLine();
                     System.out.println("insert the lastname");
@@ -41,31 +46,51 @@ public class Main {
                     sex = scanner4.nextBoolean();
                     System.out.println("insert the birth date ");
                     bDate = scanner5.nextInt();
-                    System.out.println("insert the die date");
+                    System.out.println("insert the death date");
                     dDate = scanner6.nextInt();
                     System.out.println("insert the spouse id");
                     spouseId = scanner7.nextInt();
                     adder(id, fName, lName, sex, bDate, dDate, spouseId);
                     break;
                 case 2:
-                    for (Person print : personArrayList) {
-                        System.out.println(print.getId());
-                        System.out.println(print.getfName());
-                        System.out.println(print.getlName());
-                        System.out.println(print.isSex());
-                        System.out.println(print.getbDate());
-                        System.out.println(print.getdDate());
-                        System.out.println(print.getSpouseId());
 
-
-                    }
+                    printList();
                     break;
+                case 3:
+                    int[][] connection = new int[id][id];
+                    System.out.println("connection matrix created!");
+                    break;
+                case 4:
+                    System.out.println("setting connection!");
+                    System.out.println("insert person1 and person2 to connect and choose relation");
+                    Scanner scanner8 = new Scanner(System.in);
+                    Scanner scanner9 = new Scanner(System.in);
+                    Scanner scanner10 = new Scanner(System.in);
+
+
                 default:
                     System.out.println("not in the list");
             }
 
 
         }
+    }
+
+
+    public static void printList() {
+        for (Person print : personArrayList) {
+            System.out.println("id=" + print.getId());
+            System.out.println("fisrt name=" + print.getfName());
+            System.out.println("last name=" + print.getlName());
+            System.out.println("gender=" + print.isSex());
+            System.out.println("birth date=" + print.getbDate());
+            System.out.println("death date=" + print.getdDate());
+            System.out.println("spouse id=" + print.getSpouseId());
+            System.out.println("arraylist size=" + personArrayList.size());
+            System.out.println("---------------");
+
+        }
+
     }
 
 
@@ -80,5 +105,12 @@ public class Main {
         p.setSpouseId(spouseId);
 
         personArrayList.add(p);
+    }
+    public static void relation(int id1 , int id2 , int relation){
+        Edges e = new Edges();
+        e.setFromId(id1);
+        e.setToId(id2);
+        e.setRelation(relation);
+
     }
 }
