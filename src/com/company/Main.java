9db9a6e823beb3edeffb7b3071f05aa7dbfc01c1
Dashtikh <56628256+Dashtikh@users.noreply.github.com>
2020-07-28 12:45:50 +0430

    relation completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,5 @@
 package com.company;
 
-import com.sun.javafx.css.parser.LadderConverter;
-import com.sun.javafx.geom.Edge;
-
-import javax.swing.plaf.IconUIResource;
-import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         int menu;
-        int id = 0;
+        int id = 0,id1=0,id2=0,relation=0;
         String fName;
         String lName;
         boolean sex;
@@ -26,7 +21,7 @@ public class Main {
         int spouseId;
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("insert menu item");
+            System.out.println("insert menu item: 1 add person , 2 print persons , 3 set relation , 4 print relation");
             menu = scanner.nextInt();
             switch (menu) {
                 case 1:
@@ -37,6 +32,7 @@ public class Main {
                     Scanner scanner5 = new Scanner(System.in);
                     Scanner scanner6 = new Scanner(System.in);
                     Scanner scanner7 = new Scanner(System.in);
+                    id++;
                     System.out.println("the id is=" + id);
                     System.out.println("insert the firstname");
                     fName = scanner2.nextLine();
@@ -51,21 +47,30 @@ public class Main {
                     System.out.println("insert the spouse id");
                     spouseId = scanner7.nextInt();
                     adder(id, fName, lName, sex, bDate, dDate, spouseId);
+                    if (spouseId!=0){
+                        relation(id,spouseId,2);
+                    }
                     break;
                 case 2:
-
+                    //print list
                     printList();
                     break;
                 case 3:
-                    int[][] connection = new int[id][id];
-                    System.out.println("connection matrix created!");
-                    break;
-                case 4:
-                    System.out.println("setting connection!");
-                    System.out.println("insert person1 and person2 to connect and choose relation");
+                   //set relation
+                    System.out.println("set father-son relation ( 1=p1 is son of p2)");
                     Scanner scanner8 = new Scanner(System.in);
                     Scanner scanner9 = new Scanner(System.in);
                     Scanner scanner10 = new Scanner(System.in);
+                    id1=scanner8.nextInt();
+                    id2=scanner9.nextInt();
+                    relation=scanner10.nextInt();
+                    relation(id1,id2,relation);
+
+                    break;
+                case 4:
+                    printEdgeList();
+                    break;
+
 
 
                 default:
@@ -92,6 +97,17 @@ public class Main {
         }
 
     }
+    public static void printEdgeList() {
+        for (Edges print : edgesArrayList) {
+            if (print.getRelation()==2) {
+                System.out.println(print.getToId() + " is wife of" + print.getFromId());
+            }
+            if (print.getRelation()==1) {
+                System.out.println(print.getFromId() + " is son of" + print.getToId());
+            }
+        }
+
+    }
 
 
     public static void adder(int id, String fName, String lName, boolean sex, int bDate, int dDate, int spouseId) {
@@ -106,11 +122,13 @@ public class Main {
 
         personArrayList.add(p);
     }
-    public static void relation(int id1 , int id2 , int relation){
+
+    public static void relation(int id1, int id2, int relation) {
         Edges e = new Edges();
         e.setFromId(id1);
         e.setToId(id2);
         e.setRelation(relation);
+        edgesArrayList.add(e);
 
     }
 }
